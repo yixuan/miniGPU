61c6968b8ab9d4fd622925484087a825109c6c66
Yixuan Qiu <yixuanq@gmail.com>
2016-09-03 09:33:56 -0400

    formatting

--- src/devices.cpp
+++ src/devices.cpp
@@ -12,13 +12,13 @@ bool device_info()
     std::vector<viennacl::ocl::platform> plfs = viennacl::ocl::get_platforms();
     for(std::size_t i = 0; i < plfs.size(); i++)
     {
-        Rcpp::Rcout << "***** platform " << i << " *****\n";
-        Rcpp::Rcout << plfs[i].info() << std::endl;
+        Rcpp::Rcout << "***** Platform " << i << " *****\n";
+        Rcpp::Rcout << plfs[i].info() << std::endl << std::endl;
 
         std::vector<viennacl::ocl::device> devs = plfs[i].devices(CL_DEVICE_TYPE_ALL);
         for(std::size_t j = 0; j < devs.size(); j++)
         {
-            Rcpp::Rcout << "** device " << j << " **\n";
+            Rcpp::Rcout << "=== Device " << j << " ===\n";
             Rcpp::Rcout << devs[j].info() << std::endl;
         }
 
